f3c9a5f19de481554e1f84a27490f6cb404810f5
vengy <vengyus@protonmail.com>
2025-01-04 20:59:33 -0500

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        sage: {
+          400: '#9DC7A3', // slightly lighter version
+          500: '#8ABD91'  // your specified color
+        }
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            }
+          }
+        }
+      }
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
